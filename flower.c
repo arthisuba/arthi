3a105ac97e739f18174a731630bb5d95349f9c69
arthisuba <30343815+arthisuba@users.noreply.github.com>
2017-08-26 16:55:53 +0530

    Create flower.c

---
+++ flower.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+long long n;
+int count=0;
+printf("enter the integer");
+scanf("%d",&n);
+while(n!=0)
+{ n/2=0;
+count++;
+printf("number of digit :%d,count");
+return 0;
+}
+}
